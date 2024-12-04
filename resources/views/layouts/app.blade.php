951bd9091e2447676e58fbab465b0a15752a1300
Matthias Hollerbach <mail@matthias-hollerbach.de>
2024-12-04 10:10:27 +0100

    enable gutenberg container

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,30 +9,31 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
   </head>
 
-  <body @php(body_class())>
-    @php(wp_body_open())
+<body @php(body_class())>
+  @php(wp_body_open())
 
-    <div id="app">
-      <a class="sr-only focus:not-sr-only" href="#main">
-        {{ __('Skip to content', 'sage') }}
-      </a>
+  <div id="app">
+    <a class="sr-only focus:not-sr-only" href="#main">
+      {{ __('Skip to content', 'sage') }}
+    </a>
 
-      @include('sections.header')
+    @include('sections.header')
 
-      <main id="main" class="main">
-        @yield('content')
-      </main>
+    <main id="main" class="main is-layout-constrained">
+      @yield('content')
+    </main>
 
-      @hasSection('sidebar')
-        <aside class="sidebar">
-          @yield('sidebar')
-        </aside>
-      @endif
+    @hasSection('sidebar')
+    <aside class="sidebar">
+      @yield('sidebar')
+    </aside>
+  @endif
 
-      @include('sections.footer')
-    </div>
+    @include('sections.footer')
+  </div>
 
-    @php(do_action('get_footer'))
-    @php(wp_footer())
-  </body>
+  @php(do_action('get_footer'))
+  @php(wp_footer())
+</body>
+
 </html>
