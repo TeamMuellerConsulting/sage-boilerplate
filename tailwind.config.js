d180cebba278126f217f2a8e6f0f39902197db65
Thomas Derksen <tde@team-mueller.net>
2025-04-17 10:31:10 +0200

    safelist, content, colors updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,42 @@
 import flowbite from 'flowbite/plugin.js';
+import colors from './resources/data/colors.json' assert { type: 'json' };
+
 
 /** @type {import('tailwindcss').Config} config */
 const config = {
+  mode:"jit",
   content: [
     './app/**/*.php',
     './resources/**/*.{php,vue,js}',
-    './node_modules/flowbite/**/*.js'
+    './node_modules/flowbite/**/*.js',
+    './resources/views/**/*.blade.php',
+    './resources/**/*.{js,jsx,ts,tsx,vue}',
+    './storage/framework/views/*.php',
+    './web/app/plugins/contact-form-7/**/*.php',
   ],
   theme: {
     extend: {
-      colors: {}, // Extend Tailwind's default colors
+      colors: Object.fromEntries(
+        Object.entries(colors).map(([key, value]) => [key, value.value])
+      )
     },
   },
   plugins: [
     flowbite
+  ],
+  safelist: [
+    {
+      pattern: /peer-placeholder-shown:.*/,
+      variants: [''],
+    },
+    {
+      pattern: /peer-focus:.*/,
+      variants: [''],
+    },
+    {
+      pattern: /^bg-(primary|secondary)$/,
+      variants: ['dark'],
+    }    
   ],
 };
 
