db05c0a525f76e0090f165e4e4618eac3976b607
Thomas Derksen <tde@team-mueller.net>
2025-04-17 10:50:29 +0200

    add fontawesome to head

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,37 +2,50 @@
 <html @php(language_attributes())>
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  @php(do_action('get_header'))
-  @php(wp_head())
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <script defer src="@asset('static/icons/regular.js')"></script>
+    <script defer src="@asset('static/icons/fontawesome.js')"></script>
+
+    @php(do_action('get_header'))
+    @php(wp_head())
+    <script>
+        // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+        if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia(
+                '(prefers-color-scheme: dark)').matches)) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark')
+        }
+    </script>
 </head>
 
 <body @php(body_class())>
-  @php(wp_body_open())
+    @php(wp_body_open())
 
-  <div id="app">
-    <a class="sr-only focus:not-sr-only" href="#main">
-      {{ __('Skip to content', 'sage') }}
-    </a>
+    <div id="app">
+        <a class="sr-only focus:not-sr-only" href="#main">
+            {{ __('Skip to content', 'sage') }}
+        </a>
 
-    @include('sections.header')
+        @include('sections.header')
 
-    <main id="main" class="main is-layout-constrained">
-      @yield('content')
-    </main>
+        <main id="main" class="main is-layout-constrained bg-white dark:bg-gray-800">
+            @yield('content')
+            @include('partials.form-contact')
+        </main>
 
-    @hasSection('sidebar')
-    <aside class="sidebar">
-      @yield('sidebar')
-    </aside>
-  @endif
+        @hasSection('sidebar')
+            <aside class="sidebar">
+                @yield('sidebar')
+            </aside>
+        @endif
+        @include('sections.footer')
+    </div>
 
-    @include('sections.footer')
-  </div>
-
-  @php(do_action('get_footer'))
-  @php(wp_footer())
+    @php(do_action('get_footer'))
+    @php(wp_footer())
 </body>
 
 </html>
