3a9d9c25ed24a7e15c6c46b847800cbc1d8570db
Thomas Derksen <tde@team-mueller.net>
2025-04-17 10:17:51 +0200

    navbar updated

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -1,4 +1,4 @@
-<nav class="bg-white border-gray-200 dark:border-gray-600 dark:bg-gray-900">
+{{-- <nav class="bg-white border-gray-200 dark:border-gray-600 dark:bg-gray-900">
   <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
     <a href="https://flowbite.com" class="flex items-center space-x-3 rtl:space-x-reverse">
       <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
@@ -138,4 +138,102 @@
     </li>
   @endforeach
   </ul>
-@endif
+@endif --}}
+
+@props([
+    'name' => null,
+    'inactive' => 'text-black hover:text-blue-500',
+    'active' => 'text-blue-500',
+    'ul' =>
+        'flex flex-col items-center font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-100 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-gray-100 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700',
+    'a' =>
+        'block py-2 px-3 rounded md:bg-transparent md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent',
+    'button' =>
+        'flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent',
+    'ulChild' => 'py-2 text-sm text-gray-700 dark:text-gray-400',
+    'aChild' => 'block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white',
+])
+
+@php($menu = Navi::build($name))
+
+<header class="banner">
+    <nav class="bg-gray-100 border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+                <img src="@asset('images/tmc-logo.png')" class="h-10" alt="TMC Logo" />
+            </a>
+            <button data-collapse-toggle="navbar-dropdown" type="button"
+                class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                aria-controls="navbar-dropdown" aria-expanded="false">
+                <span class="sr-only">Open main menu</span>
+                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 17 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M1 1h15M1 7h15M1 13h15" />
+                </svg>
+            </button>
+            <div class="hidden w-full md:block md:w-auto" id="navbar-dropdown">
+                @if ($menu->isNotEmpty())
+                    <ul @class([$ul]) {{ $attributes }}>
+                        @foreach ($menu->all() as $item)
+                            <li @class([
+                                $item->classes,
+                                $inactive => !$item->active,
+                                $active => $item->active,
+                            ])>
+                                @if ($item->children)
+                                    <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar"
+                                        @class([$button])>
+                                        {{ $item->label }}
+                                        <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true"
+                                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                                stroke-width="2" d="m1 1 4 4 4-4" />
+                                        </svg>
+                                    </button>
+                                @else
+                                    <a @class([$a]) href="{{ $item->url }}">
+                                        {{ $item->label }}
+                                    </a>
+                                @endif
+
+                                @if ($item->children)
+                                    <div id="dropdownNavbar"
+                                        class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+                                        <ul @class([$ulChild]) aria-labelledby="dropdownLargeButton">
+                                            @foreach ($item->children as $child)
+                                                <li @class([
+                                                    $item->classes,
+                                                    $inactive => !$item->active,
+                                                    $active => $item->active,
+                                                ])>
+                                                    <a @class([$aChild]) href="{{ $child->url }}">
+                                                        {{ $child->label }}
+                                                    </a>
+                                                </li>
+                                            @endforeach
+                                        </ul>
+                                    </div>
+                                @endif
+                            </li>
+                        @endforeach
+                        <button id="theme-toggle" type="button"
+                            class="hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5">
+                            <svg id="theme-toggle-dark-icon" class="hidden w-5 h-5" fill="currentColor"
+                                viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+                            </svg>
+                            <svg id="theme-toggle-light-icon" class="hidden w-5 h-5" fill="currentColor"
+                                viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                                    fill-rule="evenodd" clip-rule="evenodd"></path>
+                            </svg>
+                        </button>
+                    </ul>
+                @endif
+
+            </div>
+        </div>
+    </nav>
+</header>
