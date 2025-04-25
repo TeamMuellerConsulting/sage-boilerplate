6e38db2969c1cab3e0b82f93c8aa02c0c452f00d
Thomas Derksen <tde@team-mueller.net>
2025-04-25 08:53:06 +0200

    added safelist pattern

--- tailwind.config.js
+++ tailwind.config.js
@@ -34,6 +34,9 @@ const config = {
       variants: [''],
     },
     {
+      pattern: /^(mt|mb|pt|pb|pl|pr|ml|mr|text|bg|gap|grid|items|justify)-(0|1|2|3|4|5|6|8|10|12|16|20|24|32|40|48|56|64|72|80|96|px)$/,
+    },
+    {
       pattern: /^bg-(primary|secondary)$/,
       variants: ['dark'],
     }    
